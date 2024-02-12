1ffe3f89ec59a55a69a0933c68b02b4f09503b83
Calvin <cl.kleijn@student.avans.nl>
2024-02-12 12:19:15 +0100

    Add initial xUnit test projects

---
+++ DomainServicesTests/DomainServicesTests.cs
@@ -0,0 +1,16 @@
+namespace DomainServicesTests
+{
+    public class DomainServicesTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ DomainTests/DomainTests.cs
@@ -0,0 +1,16 @@
+namespace DomainTests
+{
+    public class DomainTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
