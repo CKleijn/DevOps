9ca0d044a7e0cea7a8c93a778ca7b8305d3e573b
MIKE-PC\Gebruiker <mikevandercaaij@hotmail.com>
2024-03-16 12:08:19 +0100

    + Add sprint factory base files

---
+++ DomainServices/Interfaces/ISprintFactory.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace DomainServices.Interfaces;
+
+public interface ISprintFactory
+{
+    public Sprint CreateSprint(string title, DateTime startDate, DateTime endDate, User createdBy, User scrumMaster);
+}
