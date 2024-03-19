9682eb02c727ab249af8435e6a6db0f756764c92
MIKE-PC\Gebruiker <mikevandercaaij@hotmail.com>
2024-03-19 10:22:09 +0100

    * Alter report's properties

--- Domain/Entities/Report.cs
+++ Domain/Entities/Report.cs
@@ -10,9 +10,6 @@ public class Report
     private Guid _id { get; init; }
     public Guid Id { get => _id; init => _id = value; }
     
-    private Guid _sprintId { get; init; }
-    public Guid SprintId { get => _sprintId; init => _sprintId = value; }
-    
     private string _title { get; set; }
     public string Title { get => _title; set => _title = value; }
     
@@ -22,26 +19,22 @@ public class Report
     private ReportExtension _extension { get; set; }
     public ReportExtension Extension { get => _extension; set => _extension = value; }
     
-    public Report(string title, Guid sprintId, string filePath, ReportExtension extension)
+    public Report(string title, string filePath, ReportExtension extension)
     {
         _id = Guid.NewGuid();
         _title = title;
-        _sprintId = sprintId;
         _filePath = filePath;
         _extension = extension;
 
         Logger.DisplayCreatedAlert(nameof(Report), _title);
     }
     
-    //TODO: implement functions (Update observers, etc)
-
     public override string ToString()
     {
         StringBuilder sb = new();
         
         sb.AppendLine($"Id: {_id}");
         sb.AppendLine($"Title: {_title}");
-        sb.AppendLine($"SprintId: {_sprintId}");
         sb.AppendLine($"FilePath: {_filePath}");
         sb.AppendLine($"Extension: {_extension}");
         
