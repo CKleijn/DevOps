0232dfeb3c39384d5d0f238309f4b59128ea8ec0
MIKE-PC\Gebruiker <mikevandercaaij@hotmail.com>
2024-03-23 11:25:35 +0100

    * Add thread and threadMessage tests

--- Domain.Tests/ThreadMessageTests.cs
+++ Domain.Tests/ThreadMessageTests.cs
@@ -1,6 +1,176 @@
-﻿namespace Domain.Tests;
+﻿using Domain.States.BacklogItem;
+using Thread = Domain.Entities.Thread;
+
+namespace Domain.Tests;
 
 public class ThreadMessageTests
 {
+    [Fact]
+    public void CreateThreadMessage_TitleBodyBacklogItem_WhenNoPreConditions_ThenCreateThreadMessage()
+    {
+        //Arrange
+        ISprintFactory<SprintRelease> factory = new SprintReleaseFactory();
+                
+        List<NotificationProvider> notificationProviders = new();
+        notificationProviders.Add(NotificationProvider.MAIL);
+        
+        Developer scrumMaster = new("name", "email", "password", notificationProviders);
+        IVersionControlStrategy versionControl = new GitHub();
+        ProductOwner productOwner = new("name", "email", "password", notificationProviders);
+                
+        Project project = new("Project", "Description", productOwner, versionControl);
+        SprintRelease sprint = factory.CreateSprint("Sprint", DateTime.Now, DateTime.Now.AddDays(5), scrumMaster, project);
+        
+        Item item = new("Item", "Description", scrumMaster, 1, sprint.SprintBacklog);
+        
+        Thread thread = new("Thread", "Description", item);
+        
+        //Act
+        ThreadMessage message = new("Title", "Body", thread);
+        
+        thread.AddThreadMessage(message);
+        
+        //Assert
+        Assert.NotNull(message);
+        Assert.IsType<ThreadMessage>(message);
+        Assert.NotNull(message.Thread.ThreadMessages);
+        Assert.NotNull(message.Thread);
+        Assert.Equal(1, message.Thread.ThreadMessages.Count);
+    }
     
+    [Fact]
+    public void UpdateThreadMessage_TitleBodyBacklogItem_WhenBacklogItemIsClosed_ThenDontAddMessageAndDontUpdateThreadMessage()
+    {
+        //Arrange
+        ISprintFactory<SprintRelease> factory = new SprintReleaseFactory();
+                
+        List<NotificationProvider> notificationProviders = new();
+        notificationProviders.Add(NotificationProvider.MAIL);
+        
+        Developer scrumMaster = new("name", "email", "password", notificationProviders);
+        IVersionControlStrategy versionControl = new GitHub();
+        ProductOwner productOwner = new("name", "email", "password", notificationProviders);
+                
+        Project project = new("Project", "Description", productOwner, versionControl);
+        SprintRelease sprint = factory.CreateSprint("Sprint", DateTime.Now, DateTime.Now.AddDays(5), scrumMaster, project);
+        
+        Item item = new("Item", "Description", scrumMaster, 1, sprint.SprintBacklog);
+        
+        Thread thread = new("Thread", "Description", item);
+        
+        string initialTitle = "Title";
+        string newTitle = "newTitle";
+        
+        string initialBody = "Description";
+        string newBody = "newDescription";
+        
+        //Act
+        item.CurrentStatus = new ClosedState(item);
+
+        ThreadMessage message = new(initialTitle, initialBody, thread);
+        message.Title = newTitle;
+        message.Body = newBody;
+        
+        thread.AddThreadMessage(message);
+        
+        //Assert
+        Assert.NotNull(message);
+        Assert.IsType<ThreadMessage>(message);
+        Assert.NotEqual(newTitle, message.Title);
+        Assert.Equal(initialTitle, message.Title);
+        Assert.NotEqual(newBody, message.Body);
+        Assert.Equal(initialBody, message.Body);
+        Assert.Equal(0, message.Thread.ThreadMessages.Count);
+    }
+    
+    [Fact]
+    public void UpdateThreadMessage_TitleBodyBacklogItem_WhenNoPreConditions_ThenAddMessageAndUpdateThreadMessage()
+    {
+        //Arrange
+        ISprintFactory<SprintRelease> factory = new SprintReleaseFactory();
+                
+        List<NotificationProvider> notificationProviders = new();
+        notificationProviders.Add(NotificationProvider.MAIL);
+        
+        Developer scrumMaster = new("name", "email", "password", notificationProviders);
+        IVersionControlStrategy versionControl = new GitHub();
+        ProductOwner productOwner = new("name", "email", "password", notificationProviders);
+                
+        Project project = new("Project", "Description", productOwner, versionControl);
+        SprintRelease sprint = factory.CreateSprint("Sprint", DateTime.Now, DateTime.Now.AddDays(5), scrumMaster, project);
+        
+        Item item = new("Item", "Description", scrumMaster, 1, sprint.SprintBacklog);
+        
+        Thread thread = new("Thread", "Description", item);
+        
+        string initialTitle = "Title";
+        string newTitle = "newTitle";
+        
+        string initialBody = "Description";
+        string newBody = "newDescription";
+        
+        //Act
+        ThreadMessage message = new(initialTitle, initialBody, thread);
+        message.Title = newTitle;
+        message.Body = newBody;
+        
+        thread.AddThreadMessage(message);
+        thread.RemoveThreadMessage(message);
+        thread.AddThreadMessage(message);
+        
+        //Assert
+        Assert.NotNull(message);
+        Assert.IsType<ThreadMessage>(message);
+        Assert.Equal(newTitle, message.Title);
+        Assert.NotEqual(initialTitle, message.Title);
+        Assert.Equal(newBody, message.Body);
+        Assert.NotEqual(initialBody, message.Body);
+        Assert.Equal(1, message.Thread.ThreadMessages.Count);
+    }
+    
+    [Fact]
+    public void UpdateThreadMessage_TitleBodyBacklogItem_WhenBacklogItemIsClosedAfterAddingMessage_ThenDontUpdateThreadMessage()
+    {
+        //Arrange
+        ISprintFactory<SprintRelease> factory = new SprintReleaseFactory();
+                
+        List<NotificationProvider> notificationProviders = new();
+        notificationProviders.Add(NotificationProvider.MAIL);
+        
+        Developer scrumMaster = new("name", "email", "password", notificationProviders);
+        IVersionControlStrategy versionControl = new GitHub();
+        ProductOwner productOwner = new("name", "email", "password", notificationProviders);
+                
+        Project project = new("Project", "Description", productOwner, versionControl);
+        SprintRelease sprint = factory.CreateSprint("Sprint", DateTime.Now, DateTime.Now.AddDays(5), scrumMaster, project);
+        
+        Item item = new("Item", "Description", scrumMaster, 1, sprint.SprintBacklog);
+        
+        Thread thread = new("Thread", "Description", item);
+        
+        string initialTitle = "Title";
+        string newTitle = "newTitle";
+        
+        string initialBody = "Description";
+        string newBody = "newDescription";
+        
+        //Act
+        ThreadMessage message = new(initialTitle, initialBody, thread);
+
+        thread.AddThreadMessage(message);
+        
+        item.CurrentStatus = new ClosedState(item);
+        
+        message.Title = newTitle;
+        message.Body = newBody;
+        
+        //Assert
+        Assert.NotNull(message);
+        Assert.IsType<ThreadMessage>(message);
+        Assert.NotEqual(newTitle, message.Title);
+        Assert.Equal(initialTitle, message.Title);
+        Assert.NotEqual(newBody, message.Body);
+        Assert.Equal(initialBody, message.Body);
+        Assert.Equal(1, message.Thread.ThreadMessages.Count);
+    }
 }
--- Domain.Tests/ThreadTests.cs
+++ Domain.Tests/ThreadTests.cs
@@ -1,6 +1,124 @@
-﻿namespace Domain.Tests;
+﻿using Domain.States.BacklogItem;
+using Moq;
+using Thread = Domain.Entities.Thread;
+
+namespace Domain.Tests;
 
 public class ThreadTests
 {
+    [Fact]
+    public void CreateThread_TitleBodyBacklogItem_WhenNoPreConditions_ThenCreateThread()
+    {
+        //Arrange
+        ISprintFactory<SprintRelease> factory = new SprintReleaseFactory();
+                
+        List<NotificationProvider> notificationProviders = new();
+        notificationProviders.Add(NotificationProvider.MAIL);
+        
+        Developer scrumMaster = new("name", "email", "password", notificationProviders);
+        IVersionControlStrategy versionControl = new GitHub();
+        ProductOwner productOwner = new("name", "email", "password", notificationProviders);
+                
+        Project project = new("Project", "Description", productOwner, versionControl);
+        SprintRelease sprint = factory.CreateSprint("Sprint", DateTime.Now, DateTime.Now.AddDays(5), scrumMaster, project);
+        
+        Item item = new("Item", "Description", scrumMaster, 1, sprint.SprintBacklog);
+        
+        string initialTitle = "Title";
+        string initialDescription = "Description";
+        
+        //Act
+        Thread thread = new(initialTitle, initialDescription, item);
+        
+        //Assert
+        Assert.NotNull(thread);
+        Assert.IsType<Thread>(thread);
+        Assert.Equal(initialTitle, thread.Subject);
+        Assert.Equal(initialDescription, thread.Description);
+        Assert.Equal(item, thread.Item);
+        Assert.NotNull(thread.Item);
+    }
     
+    [Fact]
+    public void UpdateThread_TitleBodyBacklogItem_WhenNoPreConditions_ThenUpdateThread()
+    {
+        //Arrange
+        ISprintFactory<SprintRelease> factory = new SprintReleaseFactory();
+                
+        List<NotificationProvider> notificationProviders = new();
+        notificationProviders.Add(NotificationProvider.MAIL);
+        
+        Developer scrumMaster = new("name", "email", "password", notificationProviders);
+        IVersionControlStrategy versionControl = new GitHub();
+        ProductOwner productOwner = new("name", "email", "password", notificationProviders);
+                
+        Project project = new("Project", "Description", productOwner, versionControl);
+        SprintRelease sprint = factory.CreateSprint("Sprint", DateTime.Now, DateTime.Now.AddDays(5), scrumMaster, project);
+        
+        Item item = new("Item", "Description", scrumMaster, 1, sprint.SprintBacklog);
+        
+        string initialTitle = "Title";
+        string newTitle = "newTitle";
+        
+        string initialDescription = "Description";
+        string newDescription = "newDescription";
+        
+        Thread thread = new(initialTitle, initialDescription, item);
+        
+        //Act
+        thread.Subject = newTitle;
+        thread.Description = newDescription;
+        
+        //Assert
+        Assert.NotNull(thread);
+        Assert.IsType<Thread>(thread);
+        Assert.Equal(newTitle, thread.Subject);
+        Assert.NotEqual(initialTitle, thread.Subject);
+        Assert.Equal(newDescription, thread.Description);
+        Assert.NotEqual(initialDescription, thread.Description);
+        Assert.Equal(item, thread.Item);
+        Assert.NotNull(thread.Item);
+    }
+    
+    [Fact]
+    public void UpdateThread_TitleBodyBacklogItem_WhenBacklogItemIsClosed_ThenDontUpdateThread()
+    {
+        //Arrange
+        ISprintFactory<SprintRelease> factory = new SprintReleaseFactory();
+                
+        List<NotificationProvider> notificationProviders = new();
+        notificationProviders.Add(NotificationProvider.MAIL);
+        
+        Developer scrumMaster = new("name", "email", "password", notificationProviders);
+        IVersionControlStrategy versionControl = new GitHub();
+        ProductOwner productOwner = new("name", "email", "password", notificationProviders);
+                
+        Project project = new("Project", "Description", productOwner, versionControl);
+        SprintRelease sprint = factory.CreateSprint("Sprint", DateTime.Now, DateTime.Now.AddDays(5), scrumMaster, project);
+        
+        Item item = new("Item", "Description", scrumMaster, 1, sprint.SprintBacklog);
+        item.CurrentStatus = new ClosedState(item);
+        
+        string initialTitle = "Title";
+        string newTitle = "newTitle";
+        
+        string initialDescription = "Description";
+        string newDescription = "newDescription";
+        
+        Thread thread = new(initialTitle, initialDescription, item);
+        
+        //Act
+        thread.Subject = newTitle;
+        thread.Description = newDescription;
+        
+        //Assert
+        Assert.NotNull(thread);
+        Assert.IsType<Thread>(thread);
+        Assert.NotEqual(newTitle, thread.Subject);
+        Assert.Equal(initialTitle, thread.Subject);
+        Assert.NotEqual(newDescription, thread.Description);
+        Assert.Equal(initialDescription, thread.Description);
+        Assert.Equal(item, thread.Item);
+        Assert.NotNull(thread.Item);
+    }
 }
